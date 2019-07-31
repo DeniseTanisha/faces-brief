4437e3f92526951cbcfb488efa50c97fb8c4a7e6
Denise Stubbs <denise.stubbs@Nology.local>
2019-07-31 08:20:49 +0100

    set-up completed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Face from './component'
+import './App.css';
+
+function App() {
+  return (
+    <div className="APP">
+        <Face leftEyeAngle={10} rightEyeAngle={20}/>
+        <Face leftEyeAngle={30} rightEyeAngle={50}/>
+
+        <p>Welcome to Shea Face</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/face.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import styles from "./face.css";
+import face from "../assets/Shea.png";
+import leftEye from "../assets/left.png";
+import rightEye from "../assets/right.png";
+import PropTypes from "prop-types";
+
+
+
+class Face extends Component {
+
+  state = {
+    normal: true
+  };
+  
+  handleClick = () => {
+    this.setState ({
+     normal: !this.state.normal
+    })
+  };
+
+  render() {
+    
+    const leftEyeRotated = this.state.normal 
+    ? {}
+    : { transform: `rotateZ(${this.props.leftEyeAngle}deg)`};
+
+    const rightEyeRotated = this.state.normal 
+    ? {}
+    : { transform: `rotateZ(${this.props.rightEyeAngle}deg)`};
+    
+    
+    return (
+          <div className={styles.container}  onClick = { this.handleClick }> 
+            <img 
+                src={face} 
+                className={styles.face} 
+                alt=""
+            />
+
+            <img 
+                src={leftEye} 
+                className={styles.leftEye} 
+                style={leftEyeRotated}
+                alt=""
+            />
+
+            <img 
+                src={rightEye} 
+                className={styles.rightEye} 
+                style= {rightEyeRotated}
+                alt="" 
+            />
+
+          </div>
+       )};
+    };
+
+                
+       Face.propTypes = {
+         leftEyeAngle: PropTypes.number.isRequired,
+         rightEyeAngle: PropTypes.number.isRequired,
+       };
+     
+   
+export default Face;
